9ffe789d08785171c70f2999de14cfb73706a0b0
Siman <siwu@roku.com>
2024-08-01 19:32:27 -0700

    created class Word and WordsDict. working on add logic for words and edges

---
+++ main.py
@@ -0,0 +1,64 @@
+import igraph as ig
+import json
+import requests
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+import numpy as np
+import plotly.express as px
+from collections import defaultdict
+
+class Word:
+    def __init__(self, text):
+        # synonyms is a list of synonyms Words
+        self.synonyms = [] 
+
+        # antonyms is a list of antonyms Words
+        self.antonyms = []
+
+        # uncategorized connection to other Words
+        self.uncatConnection = []
+
+        # the cluster it belongs to. Each Word can only have
+        # 1 cluster
+        self.cluster = None
+
+        self.text = text
+
+class WordsDict:
+    def __init__(self, wordsList, name = ""):
+        self.wordsDict =  defaultdict(None)
+        for w in wordsList:
+            self.wordsDict[w] = Word(w)
+        self.name = name
+
+    def addWordStr(self, wordStr):
+        self.wordsDict[wordStr] = Word(wordStr)
+
+    def addWordStrs(self, wordStrList):
+        for w in wordStrList:
+            self.wordsDict[w] = Word(w)
+
+    def addWordObj(self, wordObj):
+        self.wordsDict[wordObj.text] = wordObj
+
+    def printWordsDict(self):
+        dictName = self.name if self.name else "WordsDict"
+        print(f"Printing words for {dictName}:")
+        print([wordObj.text for wordObj in self.wordsDict.values()])
+
+    def getWordObj(self, wordStr):
+        return self.wordsDict[wordStr] if wordStr in self.wordsDict else None
+    
+    def getWordsStrs(self):
+        return [wordObj.text for wordObj in self.wordsDict.values()]
+    
+    
+
+    
+
+wordsList = ["repent", "atone", "amend", "insatiable"]
+
+
+synonymEdges = []
+antonymEdges = []
+uncategorizedEdges = []
