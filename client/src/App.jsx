a8810b37141f79f929bf6baef996fc72072ae141
Siman <siwu@roku.com>
2024-08-09 12:23:20 -0700

    done init focus, added a sync method to initFocus after fetchAPI got response

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SpriteText from 'three-spritetext';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 
+let initFocusId = ""
 
 function App() {
   const fg3dRef = useRef(null);
@@ -19,50 +20,22 @@ function App() {
   const fetchAPI = async () => {
     const response = await axios.get("http://127.0.0.1:8080/api/vocabnet");
     setNodesData(response.data);
+    if (response.data != null && response.data.nodes != null)
+      initFocusId = response.data.nodes[0].id
   }
 
   useEffect(() => {
     if (hasMounted.current) { return; }
     fetchAPI()
-    hasMounted.current = true;
     initFocus()
+    hasMounted.current = true;
   }, [])
 
 
   const initFocus = async () => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    fg3dRef.current.zoomToFit(3000, 10)
-  }
-
-  const focusCameraOnNode = node => {
-    // Aim at node from outside it
-    const distance = 80;
-    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
-    fg3dRef.current.cameraPosition(
-      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
-      node, // lookAt ({ x, y, z })
-      3000  // ms transition duration
-    );
-    setFocusObject(node);
-    setDescriptionData({
-      title: node.id,
-      description: node.description
-    });
-  }
-
-  const handleNodeClick = useCallback(focusCameraOnNode, []);
-
-  const setNode3ObjectStyle = node => {
-      const sprite = new SpriteText(node.id);
-      sprite.material.depthWrite = false; // make sprite background transparent
-      sprite.color = node.color;
-      sprite.textHeight = 8;
-      return sprite;
-  };
-
-  const handleDescClick = menuItem => {
-    console.log("Sidebar: clicked on description! descVisibility: ", descVisibility)
-    setDescVisibility(!descVisibility && focusObject.id != "")
+    while (initFocusId == "")
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    focusCameraById(initFocusId)
   }
 
   const cloneArray = array => {
@@ -88,10 +61,50 @@ function App() {
       var child = children.pop()
       if (child != null && child.__data != null && child.__data.id == id) 
         return child.__data
-      if (child.children != null) 
+      if (child.children != null) {
         mergeArray(children, child.children)
+      }
+        
     }
     return null
+  }
+
+  const focusCameraById = (id) => {
+    let node = findNodeDataById(id)
+    if (node == null) return 
+    focusCameraOnNode(node) 
+  }
+
+  const focusCameraOnNode = node => {
+    console.log("focus on " + node.id)
+    // Aim at node from outside it
+    const distance = 80;
+    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+    fg3dRef.current.cameraPosition(
+      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+      node, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    );
+    setFocusObject(node) 
+    setDescriptionData({
+      title: node.id,
+      description: node.description
+    });
+  }
+
+  const handleNodeClick = useCallback(focusCameraOnNode, []);
+
+  const setNode3ObjectStyle = node => {
+      const sprite = new SpriteText(node.id);
+      sprite.material.depthWrite = false; // make sprite background transparent
+      sprite.color = node.color;
+      sprite.textHeight = 8;
+      return sprite;
+  };
+
+  const handleDescClick = menuItem => {
+    console.log("Sidebar: clicked on description! descVisibility: ", descVisibility)
+    setDescVisibility(!descVisibility && focusObject.id != "")
   }
 
   // to do, add logic to refetch the data from server and focus on the node
@@ -137,6 +150,7 @@ function App() {
             nodeAutoColorBy={"group"}
             nodeThreeObject={setNode3ObjectStyle}
             nodeThreeObjectExtend={false}
+            linkAutoColorBy={"value"}
             ref={fg3dRef}
           />
         </div>
