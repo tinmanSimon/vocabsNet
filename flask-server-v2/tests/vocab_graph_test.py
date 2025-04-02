7acecd886c521f323511139c527e06403af10d7f
tinmanSimon <simonwoo2020@gmail.com>
2025-04-02 04:22:33 +0000

    added create data flow and get data flow. Currently support creating SemanticUnit, Word, and internal links. Get data only get SemanticUnit now. Also added basic test

--- flask-server-v2/tests/vocab_graph_test.py
+++ flask-server-v2/tests/vocab_graph_test.py
@@ -1,13 +1,30 @@
 import pytest
 from app.vocab_logger import logger
-from app.vocab_graph import VocabularyGraph
+from core.vocab_types import TEST_USERNAME, TEST_PWD
 
-@pytest.mark.skip(reason="Skip to not corrupt the database")
 @pytest.mark.asyncio
-async def test_add_word(client):
-    VG = VocabularyGraph()
-    await VG.add_word({
-        "name" : "asdf",
-        "description" : "owieru jaksldfj"
+async def test_add_word(client, auth_headers):
+    headers = auth_headers
+    client.post("/api/vocabnet/createdata", headers=headers, json={
+        "semantic_units": [
+            {
+                "name": "philosophy",
+                "username": TEST_USERNAME,
+                "notes": "A key concept in ancient Greece"
+            },
+            {
+                "name": "stoicism" ,
+                "username": TEST_USERNAME,
+                "notes": "I practice this daily"
+            }
+        ]
     })
-
+    response = client.get("/api/vocabnet/getdata", headers=headers)
+    assert response.status_code == 200
+    assert response.json()["user"]["username"] == TEST_USERNAME
+    assert response.json()["semantic_units"][0]["name"] == "philosophy"
+    assert response.json()["semantic_units"][0]["username"] == TEST_USERNAME
+    assert response.json()["semantic_units"][0]["notes"] == "A key concept in ancient Greece"
+    assert response.json()["semantic_units"][1]["name"] == "stoicism"
+    assert response.json()["semantic_units"][1]["username"] == TEST_USERNAME
+    assert response.json()["semantic_units"][1]["notes"] == "I practice this daily"
