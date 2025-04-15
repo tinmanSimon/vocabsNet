76fe31035f00bb5fec35ef872447744c4a449526
tinmanSimon <simonwoo2020@gmail.com>
2025-04-15 20:40:27 +0000

    revert back to mongoDB, add word and edge needs to be reimplemented

--- flask-server-v2/tests/conftest.py
+++ flask-server-v2/tests/conftest.py
@@ -4,27 +4,36 @@ from fastapi.testclient import TestClient
 from app.server import app  
 from app.vocab_logger import logger
 from core.vocab_types import TEST_USERNAME, TEST_PWD
-from core.credentials import neo4j_uri, neo4j_username, neo4j_pwd
-from neo4j import GraphDatabase
+from core.credentials import MONGO_URI, DB_NAME, DEBUG_DB_NAME
+from pymongo import MongoClient
 
 def _clear_graph(tx):
     query = "MATCH (n) DETACH DELETE n"
     tx.run(query)
 
-def clear_database():
-    logger.info(f"Starting to clear neo4j")
+def clear_database(database_name: str, connection_string: str):
+    logger.info(f"Starting to clear database: {database_name}")
+    client = MongoClient(connection_string)
+    db = client[database_name]
+
     try:
-        driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_username, neo4j_pwd))
-        with driver.session() as session:
-            session.execute_write(_clear_graph)
-        driver.close()
+        collection_names = db.list_collection_names()
+        for collection_name in collection_names:
+            logger.info(f"Dropping collection: {collection_name}")
+            db[collection_name].drop()
+        logger.info(f"Successfully cleared all collections in database: {database_name}")
+
     except Exception as e:
         logger.error(f"An error occurred while clearing the database: {e}")
+
+    finally:
+        client.close()
+        logger.info("MongoDB client closed.")
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_and_teardown_session():
     logger.info("\n-- Session Start: Setting up resources --")
-    clear_database()
+    clear_database(DEBUG_DB_NAME, MONGO_URI) 
     yield  
     logger.info("\n-- Session End: Tearing down resources --")
 
@@ -36,7 +45,7 @@ def client():
 @pytest.fixture(scope="function", autouse=True)
 def setup_and_teardown_function():
     yield 
-    clear_database() 
+    clear_database(DEBUG_DB_NAME, MONGO_URI) 
 
 @pytest.fixture(scope="function", autouse=False)
 def auth_headers(client):
