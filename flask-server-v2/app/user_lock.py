a5e48eb0cf1d251f32f6663aa5c869ca9de9f974
tinmanSimon <simonwoo2020@gmail.com>
2025-04-22 01:29:52 +0000

    added a comment on locks

--- flask-server-v2/app/user_lock.py
+++ flask-server-v2/app/user_lock.py
@@ -1,5 +1,11 @@
 import aiorwlock
 
+# This is only useful for single server single process implementation.
+# FASTAPI gives each worker its own memory space, meaning each process
+# will have a different UserLockManager. So if two workers handle the 
+# same user's requests at the same time, then we have a race condition.
+# Might want to explore distributed lock system if the project needs to 
+# scale. 
 class UserLockManager:
     def __init__(self):
         self.locks = {}
