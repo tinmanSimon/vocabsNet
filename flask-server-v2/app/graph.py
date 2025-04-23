b4e15ec0fb1b1d68f2a36db3e6e750588c16bbee
tinmanSimon <simonwoo2020@gmail.com>
2025-04-23 01:29:41 +0000

    added logic to add edges, added test cases for that, changed clear test data from directly dropping database to using server's api

--- flask-server-v2/app/graph.py
+++ flask-server-v2/app/graph.py
@@ -1,17 +1,34 @@
 from typing import Set, List, Optional
 from core.vocab_types import Word, Edge
+from typing import Dict, Tuple
 
 
 class Graph:
     def __init__(self):
         self.words: Dict[str, Word] = {}
+        self.edges: Dict[Tuple[str, str, str], Edge] = {}
 
     def add_words(self, words: list[Word]):
-        self.words = {word.name: word for word in words}
+        for word in words:
+            self.words[word.name] = word
 
     def get_all_data(self):
         return {
-            "words" : list(self.words.values())
+            "words" : list(self.words.values()),
+            "edges" : list(self.edges.values())
         }
 
+    def word_exist(self, word_name: str):
+        return word_name in self.words
+
+    def edge_exist(self, edge: Edge):
+        return (edge.edge_name, edge.from_name, edge.to_name) in self.edges
+
+    def add_edges(self, edges: list[Edge]):
+        for edge in edges:
+            from_word = self.words[edge.from_name]
+            from_word.outgoing.append(edge)
+            to_word = self.words[edge.to_name]
+            to_word.incoming.append(edge)
+            self.edges[(edge.edge_name, edge.from_name, edge.to_name)] = edge
     
--- flask-server-v2/app/graph_cache.py
+++ flask-server-v2/app/graph_cache.py
@@ -1,4 +1,7 @@
 from app.graph import Graph
+
+# Same reason as the user_lock, this only works for single server single worker
+# For now this serves its purpose, might add redis if this needs to scale.
 class GraphCacheManager:
     def __init__(self):
         self.cache: Dict[str, Optional[Graph]] = {}
@@ -19,3 +22,7 @@ class GraphCacheManager:
     async def invalidate(self, user_id: str):
         self.cache.pop(user_id, None)
         self.dirty_flags.pop(user_id, None)
+
+    async def mark_all_dirty(self):
+        self.cache = {}
+        self.dirty_flags = {}
