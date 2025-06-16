89d67f009bc3674112096177d888c514d9930595
Simon <wudisas@gmail.com>
2025-06-16 15:31:32 -0700

    fixed minor bugs on resizing

--- client-v2/vocabnet-3d-client/src/components/ModalScaffold.jsx
+++ client-v2/vocabnet-3d-client/src/components/ModalScaffold.jsx
@@ -223,7 +223,7 @@ const ModalScaffold = forwardRef(function ModalScaffold(props, ref) {
           </div>
         }
 
-        {['n', 'e', 'w', 'ne', 'nw'].map(dir => (
+        {!collapsed && showContent && ['n', 'e', 'w', 'ne', 'nw'].map(dir => (
           <div
             key={dir}
             className={`resize-handle resize-handle-${dir}`}
